4e42b7ada3c154d0bed8886599a76904666cd469
vinicius <viniciusvwh1441@gmail.com>
2023-01-21 20:55:01 -0300

    react native basics

---
+++ src/screens/Home/index.tsx
@@ -0,0 +1,35 @@
+import { Text, TextInput, TouchableOpacity, View } from "react-native";
+import { styles } from "./styles";
+
+
+export function Home() {
+
+  function handleAddParticipant() {
+    return console.log("Adicionado")
+  }
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.eventName}>
+        Nome do Evento
+      </Text>
+
+      <Text style={styles.eventDate}>
+        Sábado, 21 de Janeiro de 2023
+      </Text>
+
+      <TextInput
+        style={styles.input}
+        placeholder="Nome do participante"
+        placeholderTextColor="#6B6B6B"
+      />
+
+      <TouchableOpacity
+        style={styles.button}
+        onPress={handleAddParticipant}
+      >
+        <Text style={styles.buttonText}>+</Text>
+      </TouchableOpacity>
+    </View>
+  )
+}
---
+++ src/screens/Home/styles.ts
@@ -0,0 +1,38 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#121214'
+  },
+  eventName: {
+    color: "#FFF",
+    fontSize: 24,
+    fontWeight: "bold",
+    marginTop: 48
+  },
+  eventDate: {
+    fontSize: 16,
+    color: "#6B6B6B"
+  },
+  input: {
+    height: 56,
+    backgroundColor: '#1F1E25',
+    borderRadius: 5,
+    color: '#FFF',
+    padding: 16,
+    fontSize: 16
+  },
+  button: {
+    backgroundColor: "#31CF67",
+    height: 56,
+    width: 56,
+    borderRadius: 5,
+    alignItems: "center",
+    justifyContent: "center"
+  },
+  buttonText: {
+    color: "#FFF",
+    fontSize: 24,
+  }
+})
